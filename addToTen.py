1bfd39062445f6fc0f771f447aa634d720cc51a0
Parker Mitchell <parker.mitchell1@gmail.com>
2021-05-09 21:53:04 -0700

    stuff

--- addToTen.py
+++ addToTen.py
@@ -2,19 +2,27 @@ def addToTen():
     result = 5 + 3 + 2
     return result
 
+
+# def addToTenNEW():
+#     result = {5 + 3 + 2 + 1, 5 + 3 + 2 - 1, 5 + 3 + 2 + 0}
+#     return result
+
+
 def substitute(operation, correct):
     parsed = operation.split()
-    substitute = [1, -1, 0]
+    sub = [1, -1, 0]
     potential_result = []
 
     # assuming operations is all + and is in correct format
-    user_res = operation[2] + operation[4] + operation[6]
+    user_res = int(parsed[2]) + int(parsed[4]) + int(parsed[6])
 
-    for s in substitue:
-        res.append(user_res + s)
+    for s in sub:
+        potential_result.append(user_res + s)
 
-    for value in res:
+    for value in potential_result:
         if value == correct:
             return True
 
     return False
+
+print(substitute("v = 5 + 3 + 0", 10))
