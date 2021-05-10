1ce6818adee34b6beaf78e15c9d293e30ad58f19
Jiwon Lee <jlee671@calpoly.edu>
2021-05-09 21:10:30 -0700

    addtoten

--- addToTen.py
+++ addToTen.py
@@ -1,3 +1,20 @@
 def addToTen():
     result = 5 + 3 + 2
     return result
+
+def substitute(operation, correct):
+    parsed = operation.split()
+    substitute = [1, -1, 0]
+    potential_result = []
+
+    # assuming operations is all + and is in correct format
+    user_res = operation[2] + operation[4] + operation[6]
+
+    for s in substitue:
+        res.append(user_res + s)
+
+    for value in res:
+        if value == correct:
+            return True
+
+    return False
